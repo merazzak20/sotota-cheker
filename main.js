a8d1b2c7ec55d9caabecfe568018b30ff9e24445
merazzak <info.merazzak@gmail.com>
2024-10-31 00:51:23 +0600

    update result

--- main.js
+++ main.js
@@ -9,10 +9,26 @@ window.addEventListener("scroll", function () {
 });
 
 // JavaScript function to update the character count
+
 function updateCharacterCount() {
   const textInput = document.getElementById("textInput");
   const charCount = document.getElementById("charCount");
-  charCount.textContent = `${textInput.value.length}/10000`;
+
+  // Update character count dynamically
+  charCount.textContent = `${textInput.value.length}/1000`;
 }
+
+document.getElementById("btnPlagiarism").addEventListener("click", function () {
+  const textInput = document.getElementById("textInput").value;
+  const resultField = document.getElementById("resultField");
+  const resultFieldContainer = document.getElementById("resultFieldContainer");
+  if (textInput.length !== 0) {
+    resultFieldContainer.classList.remove("hidden");
+  } else {
+    alert("No content is Uploaded.");
+  }
+  console.log(textInput);
+  resultField.append(textInput);
+});
 
 updateCharacterCount();
